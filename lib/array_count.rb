4a43b1c59024123542da6550d097c8cfc0cf1c87
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-07 02:50:13 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,29 +1,11 @@
 def count_strings(array)
-  string_count = 0
-  index = 0
-while index < array.length do
-  if array[index].class == String
-    total += 1 
-  end
-  index += 1
-end
-total
-end
-
-def count_strings(array)
- total_strings = 0
   array.count do |element|
     element.class == String
-      total_strings += 1
-    end
-    total_strings
+end
 end
 
 def count_empty_strings(array)
- total_empty_strings = 0
   array.count do |element|
-    element == "" 
-      total_empty_strings += 1 
-    end
-    total_empty_strings
+    element == ""
+end
 end
