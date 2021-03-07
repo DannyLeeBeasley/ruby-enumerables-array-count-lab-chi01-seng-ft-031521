c09c059e6868c84ed8a25a412961b1eae92ecd81
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-07 02:39:08 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,5 +1,14 @@
 def count_strings(array)
-  
+  total = 0
+  index = 0
+ 
+while index < array.length do
+  if array[index].even?
+    total += 1 # total is only incremented when the current array element is even
+  end
+  index += 1
+end
+total
 def count_strings(array)
  total_strings = 0
   array.count do |element|
